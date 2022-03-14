3b7fdcfd9009d4ad25447e3ae5eca82f2f99aa2a
Alexandre Djun Igari <djun_igari@hotmail.com>
2022-03-14 22:04:10 +0900

    Add Banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,21 @@
+import Banner from '@components/Banner'
 import Header from '@components/Header'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
-return (
-    <div>
-        <Head>
-            <title>Airbnb Clone</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-       {/* Header */}
-       <Header />
-       {/* Banner */}
-
-    </div>
-)
+    return (
+        <div>
+            <Head>
+                <title>Airbnb Clone</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {/* Header */}
+            <Header />
+            {/* Banner */}
+            <Banner />
+        </div>
+    )
 }
 
 export default Home
